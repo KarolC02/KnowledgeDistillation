51feecd5e50decd1d1f52ddfc6e90d88daecfa0b
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-12 19:48:16 +0200

    Added ConvNeXt and resnet50 to models, with pretrained weights

--- MyCNN.py
+++ MyCNN.py
@@ -17,10 +17,12 @@ use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
 transform = transforms.Compose([
-    transforms.Resize([256,256]),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
 
 
 model_name = args.model
@@ -44,17 +46,26 @@ val_dataset = datasets.ImageFolder("datasets/tiny-imagenet-200/val/images", tran
 train_loader = DataLoader(train_dataset, batch_size = batch_size , shuffle=True, num_workers= 16, pin_memory=True)
 val_loader = DataLoader(val_dataset, batch_size = batch_size , shuffle=False, num_workers = 16, pin_memory=True)
 
+writer = SummaryWriter(log_dir=f"logs/tiny_image_net_{model_name}_lr={lr}_epochs={num_epochs}_batch_size={batch_size}")
 
-# examples = iter(val_loader)
-# example_data, example_labels = next(examples)
-# img_grid = torchvision.utils.make_grid(example_data)
-# writer.add_image('tiny_image_net_images',img_grid)
-# model.eval()
-# example_data = example_data.to(device)
-# with torch.no_grad():
-#     writer.add_graph(model, example_data)
 
-writer = SummaryWriter(log_dir=f"logs/tiny_image_net_{model_name}_lr={lr}_epochs={num_epochs}")
+# ----------------------------------
+# Pre training validation test
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in val_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+val_accuracy = 100 * correct / total
+print(f"Pre-Training Validation Accuracy: {val_accuracy:.2f}%")
+writer.add_scalar("Validation Accuracy", val_accuracy)
+# -----------------------------------------------------------
 running_loss = 0.0
 running_correct = 0
 running_total = 0
@@ -79,18 +90,6 @@ for epoch in range(num_epochs):
         running_total += labels.size(0)
 
         running_loss += loss.item()
-        # -------------------------------
-        # UNCOMMENT IF YOU WANT TO UPDATE EVERY X BATCHES
-        # -------------------------------
-
-        # if (batch) % x == 0:
-        #     accuracy = 100 * running_correct / running_total
-        #     # len(train_loader) -> number of batches in the training loader
-        #     writer.add_scalar('Training Loss', running_loss / x, epoch * len(train_loader) )
-        #     writer.add_scalar('Training Accuracy', accuracy, epoch * len(train_loader) + batch)
-        #     running_loss = 0.0
-        #     running_correct = 0
-        #     running_total = 0
 
     accuracy = 100 * running_correct / running_total
     writer.add_scalar('Training Loss', running_loss / running_total, epoch + 1 )
@@ -117,7 +116,6 @@ for epoch in range(num_epochs):
     #     print(f"Validation Accuracy in epoch {epoch}: {100 * correct / total:.2f}%")
     #     writer.add_scalar('Validation Accuracy', 100 * correct / total, epoch)
 
-writer.close()
 model.eval()
 correct = 0
 total = 0
@@ -131,5 +129,5 @@ with torch.no_grad():
 
 val_accuracy = 100 * correct / total
 print(f"Validation Accuracy: {val_accuracy:.2f}%")
-writer.add_scalar("Validation Accuracy", val_accuracy)
+writer.add_scalar("Validation Accuracy", val_accuracy, epoch)
 writer.close()
