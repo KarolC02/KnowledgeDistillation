0becc2bbc51a928f09ce9b442f439b50dcc1ad10
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-12 20:01:20 +0200

    Add lazy linear so the layers adjust to the input size

--- models/custom_model_defs.py
+++ models/custom_model_defs.py
@@ -1,4 +1,4 @@
-from torch import optim, nn
+from torch import nn
 import torch
 import torch.nn.functional as F
 
@@ -8,39 +8,39 @@ class myCNN(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 61 * 61, 500)
-        self.fc2 = nn.Linear(500, 340)
-        self.fc3 = nn.Linear(340,200)
 
+        self.fc1 = nn.LazyLinear(500)
+        self.fc2 = nn.Linear(500, 340)
+        self.fc3 = nn.Linear(340, 200)
+
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) 
-        x = nn.Dropout(0.5)(F.relu(self.fc1(x)))
-        x = nn.Dropout(0.5)(F.relu(self.fc2(x)))
+        x = torch.flatten(x, 1)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
         x = self.fc3(x)
         return x
-    
+
 class fastCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(4, 4)
-        self.fc1 = nn.Linear(63 * 63 * 6 , 200)
-
+        self.fc1 = nn.LazyLinear(200)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = torch.flatten(x, 1) 
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
         return x
 
 class OneLayerNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(256 * 256 * 3 , 200)
-
+        self.fc1 = nn.LazyLinear(200)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
