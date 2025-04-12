d3b57d219732bdd14f00ccbe74ca3c120aef02fd
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-12 19:51:32 +0200

    Added ConvNeXt and resnet 50 with pretrained weights to models

--- models/models.py
+++ models/models.py
@@ -1,9 +1,11 @@
-from torchvision.models import resnet18, ResNet18_Weights, alexnet, AlexNet_Weights
+from torchvision.models import resnet18, ResNet18_Weights, alexnet, AlexNet_Weights, ResNet50_Weights, resnet50, convnext_base, ConvNeXt_Base_Weights
 from models.custom_model_defs import myCNN, fastCNN, OneLayerNN
 
 model_dict = {
     "resnet18_pretrained": lambda: resnet18(weights=ResNet18_Weights.DEFAULT),
-    "AlexNet": lambda: alexnet(weights=AlexNet_Weights.DEFAULT),
+    "resnet50_pretrained": lambda: resnet50(weights=ResNet50_Weights.DEFAULT),
+    "AlexNet_pretrained": lambda: alexnet(weights=AlexNet_Weights.DEFAULT),
+    "ConvNeXt_pretrained": lambda: convnext_base(weights = ConvNeXt_Base_Weights.DEFAULT),
     "myCNN": myCNN,
     "fastCNN": fastCNN,
     "OneLayerNN": OneLayerNN
