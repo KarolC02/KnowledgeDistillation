3c5f6c4b53cce84d94f1aefb9217442090ba5703
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-13 16:06:02 +0200

    syntax fix

--- train_all_models.sh
+++ train_all_models.sh
@@ -3,7 +3,7 @@
 # List of models
 # models=("resnet18_pretrained" "resnet50_pretrained" "AlexNet_pretrained" "ConvNeXt_pretrained" "myCNN" "fastCNN" "OneLayerNN")
 # models=("myCNN" "fastCNN" "OneLayerNN")
-models = [vit_b_16]
+models=("vit_b_16")
 batch_size=128
 lr=0.001
 num_epochs=10
@@ -19,6 +19,7 @@ do
         --batch_size $batch_size \
         --lr $lr \
         --num_epochs $num_epochs \
+        --parallel
 
     echo "Saving $model ..."
     cp "logs/tiny_image_net_${model}_lr=${lr}_epochs=${num_epochs}_batch_size=${batch_size}/checkpoint.pth" "saved_models/${model}.pth"
