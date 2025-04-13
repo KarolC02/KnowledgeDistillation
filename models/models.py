c0b6beb6181ad635622d72556a0413f716db7deb
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-13 16:05:03 +0200

    Added Vision Transformer 16 x 16 to models

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 from torchvision.models import resnet18, ResNet18_Weights, alexnet, AlexNet_Weights, ResNet50_Weights, resnet50, convnext_base, ConvNeXt_Base_Weights
+from torchvision.models import vit_b_16, ViT_B_16_Weights
 from models.custom_model_defs import myCNN, fastCNN, OneLayerNN
 
 model_dict = {
@@ -6,6 +7,7 @@ model_dict = {
     "resnet50_pretrained": lambda: resnet50(weights=ResNet50_Weights.DEFAULT),
     "AlexNet_pretrained": lambda: alexnet(weights=AlexNet_Weights.DEFAULT),
     "ConvNeXt_pretrained": lambda: convnext_base(weights = ConvNeXt_Base_Weights.DEFAULT),
+    "vit_b_16": lambda: convnext_base(weights = ViT_B_16_Weights.DEFAULT),
     "myCNN": myCNN,
     "fastCNN": fastCNN,
     "OneLayerNN": OneLayerNN
--- train_all_models.sh
+++ train_all_models.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # List of models
-models=("resnet18_pretrained" "resnet50_pretrained" "AlexNet_pretrained" "ConvNeXt_pretrained" "myCNN" "fastCNN" "OneLayerNN")
-
+# models=("resnet18_pretrained" "resnet50_pretrained" "AlexNet_pretrained" "ConvNeXt_pretrained" "myCNN" "fastCNN" "OneLayerNN")
+# models=("myCNN" "fastCNN" "OneLayerNN")
+models = [vit_b_16]
 batch_size=128
 lr=0.001
 num_epochs=10
@@ -18,7 +19,6 @@ do
         --batch_size $batch_size \
         --lr $lr \
         --num_epochs $num_epochs \
-        --parallel
 
     echo "Saving $model ..."
     cp "logs/tiny_image_net_${model}_lr=${lr}_epochs=${num_epochs}_batch_size=${batch_size}/checkpoint.pth" "saved_models/${model}.pth"
