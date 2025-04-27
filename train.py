06d2fbd381861ebc8e2234733b836cfb25efbf77
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-04-27 14:59:55 +0200

    error fix

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ def train(args):
     if args.adapt_model:
         model = adapt_model_to_classes(model, num_classes=200)
     else:
-        adapted = "_not_addapted"
         print("Not adapting the model to 200 classes")
+
     if args.parallel:
         model = nn.DataParallel(model)
     model.to(device)
@@ -43,7 +43,7 @@ def train(args):
         args.logdir,
         args.dataset,
         args.model,
-        f"lr={args.lr:.0e}_bs={args.batch_size}_epochs={args.num_epochs}_parallel={args.parallel}{adapted}"
+        f"lr={args.lr:.0e}_bs={args.batch_size}_epochs={args.num_epochs}_parallel={args.parallel}"
     )
     os.makedirs(save_dir, exist_ok=True)
 
